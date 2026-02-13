cbcdc532b6097d4e8e14f4c652f75d5151d90ba7
Tsunami014 <tsunami014@duck.com>
2026-02-13 12:08:47 +1100

    Improved format slightly and used the api!

--- ScrapShop.md
+++ ScrapShop.md
@@ -2,3 +2,6 @@
 A shop for scraps, a ysws by hackclub.
 
 This allows you to see all the items and their odds and refinery amounts in one spot and plan for what you want to buy easily.
+
+## Running
+Ensure you have `requests` installed via pip
--- main.py
+++ main.py
@@ -1,52 +1,42 @@
+import requests
 import shutil
 import math
 
 class Item:
-    def __init__(self, name, left, base, upgr):
-        self.name: str = name
-        self.left: int = left
-        self.base = base
-        self.upgr = upgr
+    def __init__(self, dat):
+        self.data = dat
         self.got = 0
         self.want = [0, 0]
 
+    def __getitem__(self, it):
+        return self.data[it]
+
     def __repr__(self): return str(self)
     def __str__(self):
-        return f"{self.name}\n{self.left}"
+        return f"{self['name']}\n{self['count']}"
 
 
 def get_shop():
-    # amount left, (scraps, %), (upgrade scraps, upgrade %)
-    return [
-        Item("Stickers", 40,
-             (8, 79), (1, 4)),
-        Item("Daydream shirt", 13,
-             (43, 61), (10, 8)),
-        Item("applefork.mp4.mp3.mp3 cd", 10,
-             (33, 50), (11, 10)),
-    ]
+    print("Loading the shop...")
+    req = requests.get("https://api.scraps.hackclub.com/shop/items")
+    req.raise_for_status()
+    return [Item(d) for d in req.json()]
 
 def print_screen(shop):
     print("\033[2J", end="")
     size = shutil.get_terminal_size()
     strs = [str(i).split("\n") for i in shop]
 
-    cols = []
-    tmp = []
-    hei = 1
-    mxhei = size.lines
-    for it in strs:
-        h = len(it)
-        if hei + h > mxhei:
-            hei = h+1
-            if tmp: cols.append(tmp)
-            tmp = [it]
-        else:
-            hei += h + 1
-            tmp.append(it)
-    if tmp: cols.append(tmp)
+    itwids = [max(len(j) for j in i)+2 for i in strs]
+    mostwid = max(itwids)
+    colamnt = math.floor((size.columns-1) / mostwid)
+    cols = [
+        [] for _ in range(colamnt)
+    ]
+    for idx, it in enumerate(strs):
+        cols[idx%colamnt].append(it)
 
-    colwids = [max(max(len(i) for i in j) for j in c)+2 for c in cols]
+    colwids = [max(itwids[c::colamnt]) for c in range(colamnt)]
     mxrows = max(len(c) for c in cols)
     rowheis = [max(len(c[i]) for c in cols if i < len(c)) for i in range(mxrows)]
     
